6d2338621d9fb57cab3d1967c860920588b58c93
peterj140399-creator <peterj140399@gmail.com>
2025-08-18 18:39:15 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,11 @@
 import os
 import discord
-from discord import Webhook, RequestsWebhookAdapter
 
 intents = discord.Intents.default()
 intents.messages = True
 
 TOKEN = os.environ.get("DISCORD_TOKEN")
 WEBHOOK_ID = int(os.environ.get("WEBHOOK_ID"))
-WEBHOOK_TOKEN = os.environ.get("WEBHOOK_TOKEN")
-GUILD_ID = int(os.environ.get("GUILD_ID"))
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID"))
 
 client = discord.Client(intents=intents)
 
@@ -19,7 +15,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    # Ignorar mensajes del bot mismo
+    # Ignorar mensajes del propio bot
     if message.author == client.user:
         return
 
@@ -27,21 +23,12 @@ async def on_message(message):
     if message.webhook_id != WEBHOOK_ID:
         return
 
-    # Determinar el tipo de hilo según el título del embed
-    thread_name = "Sugerencia"  # valor por defecto
-    if message.embeds:
-        embed_title = message.embeds[0].title.lower()
-        if "reporte" in embed_title:
-            thread_name = "Reporte"
-        elif "sugerencia" in embed_title:
-            thread_name = "Sugerencia"
-
-    # Crear hilo en el canal donde se envió el mensaje
+    # Crear hilo siempre llamado "Sugerencia"
     if message.channel.type == discord.ChannelType.text:
-        thread = await message.channel.create_thread(
-            name=thread_name,
+        await message.channel.create_thread(
+            name="Sugerencia",
             message=message
         )
-        print(f"Hilo '{thread_name}' creado.")
+        print("Hilo 'Sugerencia' creado.")
 
 client.run(TOKEN)
